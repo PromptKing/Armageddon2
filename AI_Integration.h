f65b78542cff0b1003ce19b8fc5d07a37ab5c273
Prompt King <104260839+PromptKing@users.noreply.github.com>
2025-03-08 01:30:34 -0800

    Add files via upload

---
+++ AI_Integration.h
@@ -0,0 +1,8 @@
+﻿#ifndef AI_INTEGRATION_H
+#define AI_INTEGRATION_H
+
+#include <string>
+
+std::string QueryOllamaAI(int threadID, float computationResult, float predictedResult, float lookaheadPrediction, const std::string& modelPath);
+
+#endif // AI_INTEGRATION_H
